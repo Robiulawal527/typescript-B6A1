38b20ade88a93c7f8d2a74a2dc42d9fbe1463ffb
Robiul Awal <robiulawal527@gmail.com>
2025-11-17 18:44:07 +0600

    Remove formatValue function and add tsconfig.json for TypeScript configuration

--- solution.ts
+++ solution.ts
@@ -1,21 +1,5 @@
 
-function formatValue(value: string | number | boolean): string | number | boolean {
-    
-    if (typeof value === "string"){
-        return value.toUpperCase();
-    }
+let x = 5; 
 
-    else if ( typeof value === "number") {
-        return value * 10;
-    }
+console.log(x);
 
-    else if(typeof value === "boolean"){
-        return !value;
-    }
-
-    return value;
-
-    console.log(formatValue('hello'));
-    console.log(formatValue(5));
-    console.log(formatValue(true));
-}
