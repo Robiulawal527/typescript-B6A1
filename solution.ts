1151a9f2d8f3555bc19ce9c7fe40c253ff9ed0bf
Robiul Awal <robiulawal527@gmail.com>
2025-11-17 18:13:54 +0600

    Add formatValue function to process string, number, and boolean inputs

--- solution.ts
+++ solution.ts
@@ -1,1 +1,21 @@
 
+function formatValue(value: string | number | boolean): string | number | boolean {
+    
+    if (typeof value === "string"){
+        return value.toUpperCase();
+    }
+
+    else if ( typeof value === "number") {
+        return value * 10;
+    }
+
+    else if(typeof value === "boolean"){
+        return !value;
+    }
+
+    return value;
+
+    console.log(formatValue('hello'));
+    console.log(formatValue(5));
+    console.log(formatValue(true));
+}
